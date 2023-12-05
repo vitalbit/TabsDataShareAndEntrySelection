ef4ec16152f20492572d4dd3fcacc413f396426a
Vitali Pantsialei <vitalbit@tut.by>
2023-12-05 14:18:42 +0100

    iOS selection

---
+++ TabsDataShare/Platforms/iOS/Controls/SelectionTextField.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Platform;
+using TabsDataShare.Models;
+using UIKit;
+
+namespace TabsDataShare;
+
+public class SelectionTextField : MauiTextField
+{
+    public Command<EntryTextSelection>? OnSelectionCommand { get; set; }
+
+    public override UITextRange? SelectedTextRange
+    { 
+        get => base.SelectedTextRange;
+        set
+        {
+            var old = base.SelectedTextRange;
+
+            base.SelectedTextRange = value;
+
+            if (value != null && OnSelectionCommand != null && (old?.Start != value?.Start || old?.End != value?.End))
+            {
+                EntryTextSelection selection = new EntryTextSelection
+                {
+                     Text = this.TextInRange(value)
+                };
+                OnSelectionCommand.Execute(selection);
+            }
+        }
+    }
+}
--- TabsDataShare/Platforms/iOS/Handlers/TextSelectedEntryHandler.cs
+++ TabsDataShare/Platforms/iOS/Handlers/TextSelectedEntryHandler.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using TabsDataShare.Controls;
+using UIKit;
 
 namespace TabsDataShare.Handlers
 {
     public partial class TextSelectedEntryHandler
     {
+        public TextSelectedEntryHandler() : base()
+        {
+            Mapper.Add(nameof(TextSelectedEntry.SelectionChangedCommand), MapSelectionChangedCommand);
+        }
+
+        protected override MauiTextField CreatePlatformView()
+        {
+            TextSelectedEntry? textEntry = VirtualView as TextSelectedEntry;
+            var textField = new SelectionTextField
+            {
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                ClipsToBounds = true
+            };
+
+            if (textEntry != null)
+            {
+                textField.OnSelectionCommand = textEntry.SelectionChangedCommand;
+            }
+
+            return textField;
+        }
+
+        public static void MapSelectionChangedCommand(IEntryHandler handler, IEntry entry)
+        {
+            SelectionTextField? nativeEntry = handler.PlatformView as SelectionTextField;
+            TextSelectedEntry? textEntry = entry as TextSelectedEntry;
+            if (nativeEntry != null && textEntry != null)
+            {
+                nativeEntry.OnSelectionCommand = textEntry.SelectionChangedCommand;
+            }
+        }
     }
 }
